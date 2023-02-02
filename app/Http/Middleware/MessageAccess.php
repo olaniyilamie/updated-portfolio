9c8b20b28e5d2499d6be45f62d68cdce078eee8c
OLAMIDE <olaniyilamie@gmail.com>
2023-02-02 02:02:03 +0100

    contact form messsages middleware

---
+++ app/Http/Middleware/MessageAccess.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class MessageAccess
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!Auth::check()) 
+        {
+            return redirect()->route('login');
+        }        
+
+        if (Auth::user()->role == 'intruder' || Auth::user()->message_access == 'denied') 
+        {
+            return redirect()->route('index');            
+        }        
+            
+        return $next($request);
+    }
+}
